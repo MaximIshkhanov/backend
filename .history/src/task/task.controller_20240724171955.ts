4d5d3b84050d7f1284f1d938ca6d75443a4867cd
Maxim <m.ishkhanov@nv.dunice.net>
2024-07-25 15:15:45 +0300

    create rep

---
+++ .history/src/task/task.controller_20240724171955.ts
@@ -0,0 +1,26 @@
+import {Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create.task.dto';
+import { TaskService } from './task.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Task } from './task.model';
+
+@ApiTags('Задачи')
+@Controller('task')
+export class TaskController {
+
+    constructor(private tasksService: TaskService){}
+
+    @ApiOperation({summary:'create task'})
+    @ApiResponse({status: 200, type: Task})
+    @Post()
+    create(@Body() taskDto: CreateTaskDto) {
+        return this.tasksService.createTask(taskDto)
+    }
+
+    @ApiOperation({summary:'get all tasks'})
+    @ApiResponse({status: 200, type: [Task]})
+    @Get()
+    gerAll(){
+        return this.tasksService.getAllTasks();
+    }
+}
